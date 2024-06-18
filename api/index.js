46a30e0b41aaf9e9084254bb580a91f66bf9d802
awqsomee <awqsome@bk.ru>
2024-06-18 23:22:44 +0300

    implement vercel

--- api/index.js
+++ api/index.js
@@ -2,10 +2,7 @@ import express from "express";
 import { recipesRouter } from "./routes/recipes.js";
 
 const app = express();
-const port = process.env.PORT || 8000;
 
 app.use("/recipes", recipesRouter);
 
-app.listen(port, () => {
-  console.log(`Ready to cook on ${port}`);
-});
+export default app;
