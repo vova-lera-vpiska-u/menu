25385a48651c534516183e6a54a4df86793fc400
awqsomee <awqsome@bk.ru>
2024-06-21 17:51:42 +0300

    return base router

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,9 @@
-import { createHashRouter } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 import { publicRoutes } from './public/routes'
 import { privateRoutes } from './private/private'
 import { ProtectedRoutes } from './private/ProtectedRoutes'
 
-export const router = createHashRouter(
+export const router = createBrowserRouter(
   [
     ...publicRoutes,
     {
