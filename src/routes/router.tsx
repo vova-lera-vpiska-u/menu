9dd046f8a63c6861e5946ee4adfa73e93e58603f
awqsomee <awqsome@bk.ru>
2024-06-21 17:30:30 +0300

    Adds Github Actions

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 import { publicRoutes } from './public/routes'
 import { privateRoutes } from './private/private'
 import { ProtectedRoutes } from './private/ProtectedRoutes'
 
-export const router = createBrowserRouter(
+export const router = createHashRouter(
   [
     ...publicRoutes,
     {
