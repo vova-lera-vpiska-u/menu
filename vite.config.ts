75ebd00877d25bfc678ab2cba180d38456f5cc54
Владимир <awqsome@bk.ru>
2024-10-25 20:50:05 +0300

    feat: install babel, make readable classes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import babel from 'vite-plugin-babel'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: '/menu/',
-  plugins: [react()],
-})
+  plugins: [
+    babel(),
+    react({
+      babel: {
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            {
+              displayName: mode === 'development' ? true : false,
+            },
+          ],
+        ],
+      },
+    }),
+  ],
+}))
