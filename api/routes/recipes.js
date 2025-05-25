843a4041380c194ed2c36a4c6064c89eb4815e0a
Владимир <awqsome@bk.ru>
2025-05-26 00:46:21 +0300

    fix: recipes

--- api/routes/recipes.js
+++ api/routes/recipes.js
@@ -8,12 +8,13 @@ import { v4 as uuidv4 } from "uuid";
 
 import multer from "multer";
 import { Section } from "../models/section.js";
+import { Category } from "../models/category.js";
 
 const upload = multer({ storage: multer.memoryStorage() });
 
 export const recipesRouter = new Router();
 
-recipesRouter.post("/", upload.single("image"), async (req, res) => {
+recipesRouter.post("/", expressjwt(jwt), upload.single("image"), async (req, res) => {
   const file = req.file;
   const { name, section, categories, rating, timeToCook, nutrition } = req.body;
   const nutritionParsed = JSON.parse(nutrition);
@@ -36,16 +37,21 @@ recipesRouter.post("/", upload.single("image"), async (req, res) => {
     nutrition: nutritionParsed,
   });
   await recipe.save();
+
   await Section.findOneAndUpdate({ _id: section }, { $push: { recipes: recipe._id } });
+  await Category.updateMany({ _id: { $in: categoriesParsed } }, { $push: { recipes: recipe._id } });
   res.send(recipe).status(200);
 });
 
 recipesRouter.get("/", async (req, res) => {
-  const recipes = await Recipe.find().populate("categories ingredients");
+  const { name } = req.query;
+  const recipes = await Recipe.find(name ? { name: { $regex: name, $options: "i" } } : {}).populate(
+    "categories ingredients section"
+  );
   res.send(recipes).status(200);
 });
 recipesRouter.get("/:id", async (req, res) => {
-  const recipes = await Recipe.findById(req.params.id).populate("categories ingredients");
+  const recipes = await Recipe.findById(req.params.id).populate("categories ingredients section");
   res.send(recipes).status(200);
 });
 recipesRouter.put("/:id", expressjwt(jwt), async (req, res) => {
